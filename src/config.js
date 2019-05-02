17c6b132f8ec50d07cc1bf14f737309ebc351561
Joel Corey <joelcorey@fastmail.com>
2019-05-02 06:49:37 -0700

    stub init pages, services, api endpoints

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default {
+  STOPS_API_ENDPOINT: 'http://localhost:8000/api/stops',
+  TOKEN_KEY: 'blogful-client-auth-token',
+}
---
+++ src/services/bus-stop-api-service.js
@@ -0,0 +1,29 @@
+import TokenService from './token-service'
+import config from '../config'
+
+const BusStopApiService = {
+  getStops() {
+    return fetch(`${config.STOPS_API_ENDPOINT}/`, {
+      headers: {
+      },
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+  getTimesAtStop(stopId, time) {
+    // Non-exact endpoint for at this time and after this time
+    return fetch(`${config.STOPS_API_ENDPOINT}/times/${stopId}/${time}`, {
+      headers: {
+      },
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  }
+
+}
