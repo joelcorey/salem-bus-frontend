9a6d807ead1aa8c5c10212b29739def93fa58463
Joel Corey <joelcorey@fastmail.com>
2019-05-02 09:06:19 -0700

    stub components and routes

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
 import './App.css'
-
 import Loading from '../Loading/Loading'
 import Message from '../Message/Message'
 import Header from '../Header/Header'
+import PrivateRoute from '../Utils/PrivateRoute'
+import PublicOnlyRoute from '../Utils/PublicOnlyRoute'
+import StopListPage from '../../routes/StopListPage/StopListPage'
+import LoginPage from '../../routes/LoginPage/LoginPage'
+import RegistrationPage from '../../routes/RegistrationPage/RegistrationPage'
+import NotFoundPage from '../../routes/NotFoundPage/NotFoundPage'
 
 export default class App extends Component {
   state = {
@@ -21,9 +27,28 @@ export default class App extends Component {
         <main className="App">
           <Message 
             message={this.state.message}
-            isActive={true}
+            isActive={false}
           />
           
+          <Switch>
+            <Route
+              exact
+              path={'/'}
+              component={StopListPage}
+            />
+            <PublicOnlyRoute
+              path={'/login'}
+              component={LoginPage}
+            />
+            <PublicOnlyRoute
+              path={'/register'}
+              component={RegistrationPage}
+            />
+            <Route
+              component={NotFoundPage}
+            />
+          </Switch>
+
         </main>
       </>
 
