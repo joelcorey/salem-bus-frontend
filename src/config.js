95e52f8976085ca80c055b5927ca63403bf6fe50
Joel Corey <joelcorey@fastmail.com>
2019-05-12 16:18:41 -0700

    remote API endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   STOPS_API_ENDPOINT: 'http://localhost:8000/api/stops',
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://calm-ridge-94950.herokuapp.com/api',
   TOKEN_KEY: 'salembus-client-auth-token',
   FIRST_LOAD_MESSAGE: 'Please enter a stop above',
 }
