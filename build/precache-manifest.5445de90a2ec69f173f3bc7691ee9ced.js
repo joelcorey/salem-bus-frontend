d025ef10be652a717e5ac136d86f6e0df9712f15
Joel Corey <joelcorey@fastmail.com>
2019-05-12 10:50:36 -0700

    add deploy scripts

---
+++ build/precache-manifest.5445de90a2ec69f173f3bc7691ee9ced.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b04a38f9e9915d8ff44b9331df5ebc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "345b728e9b473bdc15cd",
+    "url": "/static/css/main.fced714a.chunk.css"
+  },
+  {
+    "revision": "796526d1239f6a3e6f41",
+    "url": "/static/js/2.c3f43162.chunk.js"
+  },
+  {
+    "revision": "345b728e9b473bdc15cd",
+    "url": "/static/js/main.5a192471.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bbf639060ebc175fea350ada3d4a5f7b",
+    "url": "/static/media/loading.bbf63906.jpg"
+  }
+]);
